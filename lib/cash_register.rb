6621fffa8c6a7d40cd23f2fa3a458e3053af6e36
Sharon Watkins <swatkins876@gmail.com>
2019-10-04 15:03:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,5 +8,18 @@ class CashRegister
     @items = []
   end 
   
-  def add_items
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
 end 
