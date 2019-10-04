dfcdbb3ee0d16f4b754cb5d74151a2bb85029131
Sharon Watkins <swatkins876@gmail.com>
2019-10-04 15:23:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
     @price = price 
-    self.total = price * quantity
+    @total += price * quantity
     @items << title 
   end
   
